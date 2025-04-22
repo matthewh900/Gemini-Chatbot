b7befe057bec7a4759f307cd75c2d99b43dbd355
matthewh900 <matthewh900@gmail.com>
2025-04-22 17:14:37 +0100

    add use state and use effect to manipulate and generate response

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,26 @@
-import { Text, View } from "react-native";
+import { GoogleGenAI } from "@google/genai";
+import { useEffect, useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [res, setRes] = useState<string | undefined>("")
+  const [prompt, setPrompt] = useState<string>("")
+  const [pressed, setPressed] = useState<boolean>(false)
+  
+  const ai = new GoogleGenAI({})
+
+  async function main() {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: prompt || "say hello to me",
+    });
+    setRes(response.text);
+  }
+
+  useEffect(() => {
+    main()
+  }, [pressed])
+
   return (
     <View
       style={{
@@ -9,7 +29,16 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{res}</Text>
+      <TextInput
+      onChangeText={setPrompt}
+      value={prompt}
+      placeholder="Ask me a question"
+      />
+      <Button
+      title="Enter"
+      onPress={() => setPressed}
+      />
     </View>
   );
 }
