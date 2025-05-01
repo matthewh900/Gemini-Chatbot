30d1d936cbe2b198d6d2656cf14d4dc247b4a159
matthewh900 <matthewh900@gmail.com>
2025-05-01 16:24:34 +0100

    add messages useState and create array of responses with no ts errors

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,27 @@
 import { GoogleGenAI } from "@google/genai";
 import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Button,
-  Text,
-  TextInput,
-} from "react-native";
+import { ActivityIndicator, Button, Text, TextInput } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function Index() {
-  const [res, setRes] = useState<string | undefined>(""); //change this to array of responses
+  const [res, setRes] = useState<string | undefined>("");
+  const [messages, setMessages] = useState<string[]>([]);
   const [prompt, setPrompt] = useState<string>("");
   const [pressed, setPressed] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
   const ai = new GoogleGenAI({
+   
   });
 
   async function main() {
-    const response = await ai.models.generateContent({
+    type MyResponse = { text: string };
+    const response = (await ai.models.generateContent({
       model: "gemini-2.0-flash",
       contents: prompt || "say hello to me",
-    });
+    })) as MyResponse;
     setRes(response.text); // make this append response to the end of array
+    setMessages((prevMessages) => [...prevMessages, response.text]);
   }
 
   useEffect(() => {
@@ -33,22 +32,23 @@ export default function Index() {
   }, [pressed]);
 
   return (
-   
-      <KeyboardAwareScrollView contentContainerStyle={{
+    <KeyboardAwareScrollView
+      contentContainerStyle={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
         paddingTop: 30,
         paddingBottom: 40,
-      }}>
-        {/* make this use flatlist using chatbubble to renders item */}
-        {loading ? <ActivityIndicator size="large" /> : <Text>{res}</Text>} 
-        <TextInput
-          onChangeText={setPrompt}
-          value={prompt}
-          placeholder="Ask me a question"
-        />
-        <Button title="Enter" onPress={() => setPressed(!pressed)} />
-      </KeyboardAwareScrollView>
+      }}
+    >
+      {/* make this use flatlist using chatbubble to renders item */}
+      {loading ? <ActivityIndicator size="large" /> : <Text>{res}</Text>}
+      <TextInput
+        onChangeText={setPrompt}
+        value={prompt}
+        placeholder="Ask me a question"
+      />
+      <Button title="Enter" onPress={() => setPressed(!pressed)} />
+    </KeyboardAwareScrollView>
   );
 }
